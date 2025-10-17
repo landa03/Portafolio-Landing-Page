4b843fd24c10d08951a91df1347cf67360728287
landita03 <omarlandita03@gmail.com>
2025-10-16 17:03:03 -0800

    poco avance al header

--- public/Header.jsx
+++ public/Header.jsx
@@ -1,7 +1,7 @@
 import gsap from "gsap"
 import { useGSAP } from '@gsap/react';
 
-import './Header.css'
+import './Header.scss'
 import { Timeline } from 'gsap/gsap-core';
 // gsap.registerPlugin(useGSAP); 
 
