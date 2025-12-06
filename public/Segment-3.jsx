93524ff2a3ec4ebd5d6e3452aa64be8d5b890456
landita03 <omarlandita03@gmail.com>
2025-12-05 18:55:25 -0800

    termine la primer parte de las skill bars

--- public/Segment-3.jsx
+++ public/Segment-3.jsx
@@ -6,7 +6,19 @@ function Segment3() {
   return (
     <div className='segment-3-body'>
       <div className='segment-title'>My Skills and Advantages</div>
-      <SkillBar />
+      <SkillBar skillName = "HTML" maxProgres = "5" curProgres = "5" />
+      <SkillBar skillName = "CSS" maxProgres = "5" curProgres = "5" />
+      <SkillBar skillName = "JS" maxProgres = "5" curProgres = "4" />
+      <SkillBar skillName = "TS" maxProgres = "5" curProgres = "1" />
+      <SkillBar skillName = "React" maxProgres = "5" curProgres = "2" />
+      <SkillBar skillName = "SASS" maxProgres = "5" curProgres = "2" />
+      <SkillBar skillName = "Vite" maxProgres = "5" curProgres = "3" />
+      <SkillBar skillName = "Git" maxProgres = "5" curProgres = "4" />
+      <SkillBar skillName = "REST APIs" maxProgres = "5" curProgres = "3" />
+      <SkillBar skillName = "C#" maxProgres = "5" curProgres = "3" />
+      <SkillBar skillName = "Dart" maxProgres = "5" curProgres = "2" />
+      <SkillBar skillName = "Pyithon" maxProgres = "5" curProgres = "1" />
+      <SkillBar skillName = "GD Scripts" maxProgres = "5" curProgres = "1" />
     </div>
   );
 }
--- public/Skill-bar.jsx
+++ public/Skill-bar.jsx
@@ -2,22 +2,30 @@ import './Skill-bar.scss'
 import { useRef } from 'react'
 
 
-function SkillBar({skillName = "Skill", maxProgres = "5", curProgres = "3"}) {
+function SkillBar({skillName = "Skill", maxProgres = "5", curProgres = "1"}) {
 
   // 100% - sbpb (?)
   // mP - cP
   // (cP/mP)*100 = sbpb
+  let timeMesure = "years"
 
-  let skillBarWidth = useRef(); 
+  if (curProgres > 1) {
+    timeMesure = "years"
+  } else{
+    timeMesure = "year"
+  }
+
+  let skillBarRef = useRef(); 
+  let skillNameRef = useRef(); 
     return (
-      <div className='skill-bar' ref={skillBarWidth}>
-        <div className='skill-bar-progres-bar'>
-          <div className='skill-name' 
-            style={{width: `${(curProgres/maxProgres)*100}%`, minWidth: `fit-content`}}> 
+      <div className='skill-bar' ref={skillBarRef}>
+        <div className='skill-bar-progres-bar'
+        style={{width: `${(curProgres/maxProgres)*100}%`, minWidth: `fit-content`, maxWidth: `${skillBarRef.width - skillNameRef.width}`}}>
+          <div className='skill-name' ref={skillNameRef} >
             {skillName} 
           </div>
         </div>
-        <div className='skill-bar-progres-text'>{curProgres} years</div>
+        <div className='skill-bar-progres-text'>{curProgres} {timeMesure}</div>
       </div>
     );
 }
